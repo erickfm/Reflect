9bd31a5e09c0d3f19de52f66666e9a98f1568a1b
Erick <efm.physics@gmail.com>
2023-06-11 17:54:56 -0700

    photo url fix

--- reflect/constants.py
+++ reflect/constants.py
@@ -1,3 +1,3 @@
-reflect_image_path = 'https://raw.githubusercontent.com/erickfm/Reflect/main/images/reflect.png'
+reflect_image_path = 'https://raw.githubusercontent.com/erickfm/Reflect/main/images/Reflect.png'
 github_image_path = 'https://raw.githubusercontent.com/erickfm/Reflect/main/images/github-mark.png'
 patreon_image_path = 'https://raw.githubusercontent.com/erickfm/Reflect/main/images/Digital-Patreon-Logo_Black.png'
